e5428d1831aa1bea99083c89487acb92845b6159
HusseinMohssen <hussein.mohsen100@gmail.com>
2024-02-10 03:59:09 +0200

    sec commit

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import Home from './components/Home/Home'
 import About from './components/About/About'
 import ContactUs from './components/ContactUs/ContactUs'
 import Portfolio from './components/Portfolio/Portfolio'
-import {RouterProvider, createBrowserRouter} from 'react-router-dom'
+import {RouterProvider, createBrowserRouter, createHashRouter} from 'react-router-dom'
 import UserLayout from './Layouts/UserLayout/UserLayout'
 export default function App() {
-    let routes = createBrowserRouter([
+    let routes = createHashRouter([
         {
           path: '/', element: <UserLayout/>, children:[
           {index: true , element: <Home/>},
